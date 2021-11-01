4103eebf017149574d393edcab86a972f4909dde
DownTheMatrix <suija87@gmail.com>
2021-11-01 11:40:38 +0100

    Only render movies with posters, adjust overall styles.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,39 +1,28 @@
 import React, { Component } from "react";
 import "./MovieCard.css";
-import poster from "../images/poster-unavailable.svg";
 
 class MovieCard extends Component {
   /* Display movie cards */
   displayMovieCards() {
-    if (this.props.poster === null) {
-      return (
-        <figure className="MovieCard-box">
-          <img
-            className="MovieCard-poster"
-            src={poster}
-            alt="Poster Unavailable"
-            title="Poster Unavailable"
-          />
-          <figcaption className="MovieCard-info">
-            <h3 className="MovieCard-alt-heading">
-              The movie poster and info are temporarily not available.
-            </h3>
-          </figcaption>
-        </figure>
-      );
-    } else {
-      return (
-        <figure className="MovieCard-box">
+    return (
+      this.props.poster && (
+        <figure className="movieCard-box">
           <img
             className="MovieCard-poster"
             src={`https://image.tmdb.org/t/p/w342/${this.props.poster}`}
             alt={`${this.props.title} Poster`}
-            title={`${this.props.title}`}
+            title={this.props.title}
           />
           <figcaption className="MovieCard-info">
             <h3 className="MovieCard-title">{this.props.title}</h3>
             <span className="MovieCard-date">{this.props.releaseDate}</span>
-            <p className="MovieCard-summary">{this.props.overview}</p>
+            {this.props.overview.length ? (
+              <p className="MovieCard-summary">{this.props.overview}</p>
+            ) : (
+              <p className="MovieCard-summary">
+                The movie summary is not available.
+              </p>
+            )}
             <button
               className="MovieCard-btn"
               onClick={() => this.props.viewMovieInfo(this.props.movieId)}
@@ -42,12 +31,19 @@ class MovieCard extends Component {
             </button>
           </figcaption>
         </figure>
-      );
-    }
+      )
+    );
   }
 
   render() {
-    return <div className="MovieCard">{this.displayMovieCards()}</div>;
+    /* return <div className="MovieCard">{this.displayMovieCards()}</div>; */
+    {
+      return this.props.poster ?  (
+       <div className="MovieCard">{this.displayMovieCards()}</div>
+      ) : (
+        <div className="MovieCard-hidden"></div>
+      );
+    }
   }
 }
 
--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -4,43 +4,30 @@ import poster from "../images/poster-unavailable.svg";
 
 class MovieInfo extends Component {
   displayMovieInfo() {
-    if (this.props.poster === null) {
-      return (
-        <div className="MovieInfo-container">
-          <figure className="MovieInfo-box">
-            <img
-              className="MovieInfo-poster"
-              src={poster}
-              alt="Poster Unavailable"
-              title="Poster Unavailable"
-            />
-            <figcaption className="MovieInfo-details">
-              <h3 className="MovieInfo-alt-title">
-                The movie poster and info are temporarily not available.
-              </h3>
-            </figcaption>
-          </figure>
-        </div>
-      );
-    } else {
+    {
       return (
         <div className="MovieInfo-container">
           <div className="Movieinfo-box">
             <img
               className="MovieInfo-poster"
-              src={`https://image.tmdb.org/t/p/w342/${
-                this.props.currentMovie.poster_path
-              }`}
+              src={`https://image.tmdb.org/t/p/w342/${this.props.currentMovie.poster_path}`}
               alt={`${this.props.title} Poster`}
               title={`${this.props.title}`}
             />
-            </div>
-            <div className="MovieInfo-details">
-              <h3 className="MovieInfo-title">{this.props.currentMovie.title}</h3>
-              <h4 className="MovieInfo-date">{this.props.currentMovie.release_date}</h4>
-              <p className="MovieInfo-summary">{this.props.currentMovie.overview}</p>
-              <p className="MovieInfo-rating"><span className="MovieInfo-rating-label">Average Vote: </span>{this.props.currentMovie.vote_average}</p>
-            </div>
+          </div>
+          <div className="MovieInfo-details">
+            <h3 className="MovieInfo-title">{this.props.currentMovie.title}</h3>
+            <h4 className="MovieInfo-date">
+              {this.props.currentMovie.release_date}
+            </h4>
+            <p className="MovieInfo-summary">
+              {this.props.currentMovie.overview}
+            </p>
+            <p className="MovieInfo-rating">
+              <span className="MovieInfo-rating-label">Average Vote: </span>
+              {this.props.currentMovie.vote_average}
+            </p>
+          </div>
         </div>
       );
     }
